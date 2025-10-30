dddbc3b969ced2f075058f6cf43e7a8d706a6bd1
Vinayak S <vinayaksukhalal@gmail.com>
2025-10-30 11:28:22 +0530

    deployment

--- src/main.tsx
+++ src/main.tsx
@@ -8,11 +8,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    
-
     <QueryClientProvider client={queryClient}>
     <App />
     </QueryClientProvider>
-
   </StrictMode>,
 )
